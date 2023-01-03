048e46fdfa790b9452007a2ac40fb32c8e4baaa1
Aleksey Andreev <fistashki22@gmail.com>
2023-01-03 21:15:03 +0300

    add check.php

---
+++ check.php
@@ -0,0 +1,30 @@
+<?php
+$login = filter_var(
+    trim($_POST['login']),
+FILTER_SANITIZE_STRING
+);
+$name = filter_var(
+    trim($_POST['name']),
+FILTER_SANITIZE_STRING
+);
+$pass = filter_var(
+    trim($_POST['pass']),
+FILTER_SANITIZE_STRING
+);
+
+if (mb_strlen($login) < 5 || mb_strlen($login) > 90) {
+    echo "Длина логина должна быть не менее 5 символов";
+    exit();
+} else if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+    echo "Длина имени должна быть не менее 3 и не более 50 символов";
+    exit();
+} else if (mb_strlen($pass) < 3 || mb_strlen($pass) > 20) {
+    echo "Длина пароля должна быть не менее 3 и не более 20 символов";
+    exit();
+}
+
+$mysql = new mysqli('localhost', 'root', '', 'park');
+$mysql->query('INSERT INTO `users` (user_id, login, pass, name)
+VALUES($login, $pass, $name)');
+$mysql->close();
+?>
--- index.php
+++ index.php
@@ -11,6 +11,18 @@
 
 <body>
     <div class="container my-5">
+        <h1>Форма регистрации</h1>
+        <form action="check.php" method="post">
+            <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Введите имя"><br>
+            <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль"><br>
+            <button class="btn btn-success" type="submit">Зарегистрировать</button>
+        </form>
+    </div>
+
+
+
+    <div class="container my-5">
         <h2>Мероприятия</h2>
         <a class="btn btn-primary" href="/park/activities/create.php" role="button">Добавить</a>
         <br>
